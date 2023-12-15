413faecbecfc81d599b02461517b454203893f21
Nguyenkhoi196 <nguyenkhoi196@gmail.com>
2023-12-15 17:29:21 +0700

    updat mai

--- trang-gioi-thieu.js
+++ trang-gioi-thieu.js
@@ -1,4 +1,7 @@
-console.log('trang giới thiệu đã hoàn thành');
-console.log('update')
-console.log('update2')
-console.log('update from main1')
+console.log("trang giới thiệu đã hoàn thành");
+console.log("update");
+console.log("update2");
+console.log("update from main1");
+console.log("trang giới thiệu đã hoàn thành");
+console.log("update");
+console.log("update3");
